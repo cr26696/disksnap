91c48e971ca18470cda0298ac4560d7f288bf68b
cr26696 <chenr26696@gmail.com>
2025-03-12 19:54:35 +0800

    refactor(main): 卡卡的初版
    - 添加调试宏定义，用于打印变量和日志信息，之后可分一个branch出来
    - 修了明显的bug,现在能跑，就是很卡，官网上传会超时

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <unordered_set>
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -17,13 +18,24 @@ using namespace std;
 #define REP_NUM (3)
 #define FRE_PER_SLICING (1800)
 #define EXTRA_TIME (105)
+#define DEBUG_ENABLED 0
+#define DEBUG_PRINT(var)                                                                                      \
+    if (DEBUG_ENABLED)                                                                                        \
+    {                                                                                                         \
+        std::cout << "[D] " << #var << " = " << (var) << " (in " << __PRETTY_FUNCTION__ << ")" << std::endl; \
+    }
+#define DEBUG_LOG(msg)                       \
+    if (DEBUG_ENABLED)                       \
+    {                                        \
+        std::cout << "[D]: " << msg << endl; \
+    }
 
 typedef struct Request_
 {
     int object_id;
     int prev_id;
     bool is_done;
-    bool complete[6]={0};
+    bool complete[6] = {0};
 } Request;
 
 typedef struct Object_
@@ -34,70 +46,82 @@ typedef struct Object_
     int last_request_point; // 最后一次请求任务
     bool is_delete;
 } Object;
-
+// TODO 将对象和对象副本解耦
 Request request[MAX_REQUEST_NUM];
 Object object[MAX_OBJECT_NUM];
 int T, M, N, V, G;                     // 时间片T 标签M 硬盘N 存储单元数V 磁头tokenG
 int disk[MAX_DISK_NUM][MAX_DISK_SIZE]; // 磁盘内容
 int disk_point[MAX_DISK_NUM];          // 磁头位置
-// TODO 任务调度器 读取当前任务列表，计算出待查找块，调用disk.task 具体操作磁头读取
 // 收集返回结果  统计已完成任务，最终统一上报
 class Scheduler
 {
 public:
-    unordered_set<int> active_requests; //TODO 改成map（对象编号，请求当前对象的所有请求id）
+    unordered_set<int> active_requests; // OPT 改成map（对象编号，请求当前对象的所有请求id）
 private:
 public:
     // 成功失败
     bool add_request(int req_id)
     {
-        active_requests.insert(req_id);
+        pair<unordered_set<int>::iterator, bool> result = active_requests.insert(req_id);
+        return result.second;
     }
-    bool del_request(int req_id){
-        active_requests.erase(req_id);
+    bool del_request(int req_id)
+    {
+        return active_requests.erase(req_id);
     }
+    // 返回的vector从0开始索引
     vector<int> get_task_for_disk(int disk_id)
     {
-        // TODO 获取一个对应块的数组
         vector<int> target;
-        target.push_back(-1);
-        
+        // target.push_back(-1);
+
         for (int req_id : active_requests)
         {
-            
+
             Request req = request[req_id];
             Object obj = object[req.object_id];
-            for (int i=1;i<=3;i++)
+
+            for (int i = 1; i <= 3; i++)
             {
                 if (obj.replica[i] == disk_id)
                 {
-                    int* units = obj.unit[i];
-                    for(int i=1;i<=obj.size;i++){
+                    int *units = obj.unit[i];
+                    for (int i = 1; i <= obj.size; i++)
+                    {
                         target.push_back(units[i]);
                     }
                     break;
                 }
             }
         }
-        sort(target.begin()+1,target.end());
-        
+        sort(target.begin(), target.end());
+        DEBUG_PRINT(target.size());
         return target;
     }
-    void req_upload(){
+    void req_upload()
+    {
         int complete_num = 0;
         string info = "";
-        bool complete_flag = 1;
-        for(auto actreq_id:active_requests){
-            complete_flag = 1;
-            for(int i = 1; i<=object[request[actreq_id].object_id].size; i++){
-                if(request[actreq_id].complete[i] != true) complete_flag = 0;
+        bool complete_flag;
+
+        for (auto actreq_id : active_requests)
+        {
+            complete_flag = true; // 初始化为true
+            for (int i = 1; i <= object[request[actreq_id].object_id].size; i++)
+            {
+                if (request[actreq_id].complete[i] != true)
+                    complete_flag = false; // 如果有不完整的，设置为false
             }
-            if(complete_flag == 1){
+            if (complete_flag)
+            {
                 complete_num++;
-                info+= actreq_id + "\n";
+                del_request(actreq_id);
+                request[actreq_id].is_done = true;
+                info += std::to_string(actreq_id) + "\n"; // 转换为字符串
             }
         }
-        info = complete_num+"\n" + info;
+        info = std::to_string(complete_num) + "\n" + info; // 转换complete_num为字符串
+        cout << info;
         return;
     }
 };
@@ -110,7 +134,7 @@ enum DiskOp
 class Disk
 {
 
-    int head = 1;    // 初始为1
+    int head = 0;    // 初始为0 如size == 8000 可能的值为0~7999
     int head_s = -1; // 磁头上个操作 -1初始化 0刚jump过 >0
     // 64刚read过 52=ceil(64*0.8) ... 16 上次连续read过
     // 1 pass过
@@ -119,26 +143,30 @@ class Disk
     int token;                 // 当前时间片总可用token数
     int elapsed = 0;           // 当前时间片已用token数
     int phase_end = false;
+    // OPT 是否可以维护一个hash表 键为obj_id 值为一些关键信息 如各块存放位置
 
 private:
     // 会移动head JUMP需传入跳转地址 PASS传入距离 READ无视参数可填0
-    // 返回操作是否进行了操作
+    // 返回操作是否成功 pass 0也算成功
     bool operate(DiskOp op, int param)
     {
+        // DEBUG_PRINT(op);
+        // DEBUG_PRINT(param);
         int consume_token = 0;
-        
+
         switch (op)
         {
         case JUMP:
+            DEBUG_LOG("ready jump");
             if (elapsed > 0)
             {
                 phase_end = true;
                 return false;
             }
-            printf("j %d\n", param);
-            head = (param-1)%V+1;
+            printf("j %d\n", param+1);
+            head = param % V;
             head_s = 0;
-            elapsed = token;
+            elapsed = token+1;
             phase_end = true;
             break;
         case PASS:
@@ -147,24 +175,28 @@ private:
             {
                 if (elapsed + param > token)
                 {
-                    // operate(PASS,token-elapsed)//直接移动最大距离？
                     phase_end = true;
-                    return false; // 或者调用end？
+                    return false;
                 }
                 string s = string(param, 'p');
                 printf("%s", s.c_str()); // 使用 c_str() 将 std::string 转换为 const char*
                 head += param;
-                head = (head - 1) % size + 1;
+                head = head % size;
                 head_s = 1;
-                elapsed += param > 2 ? param : 1;
+                elapsed += param;
+                return true;
             } // pass 0等于不做动作
+            else if(param==0) return true;
+            return false;
             break;
         case READ:
+            // DEBUG_PRINT(head_s);
+            // DEBUG_PRINT(elapsed);
+            // DEBUG_PRINT(token);
             consume_token = 64;
             if (head_s > 16)
                 consume_token = head_s;
-
-            if (elapsed + consume_token > token)
+            if (elapsed + consume_token >= token)
             {
                 phase_end = true;
                 return false;
@@ -172,8 +204,9 @@ private:
             else
             {
                 elapsed += consume_token;
-                head_s = consume_token;
+                head_s = ceil(consume_token*0.8);
                 printf("r");
+                head++;
             }
             break;
         default:
@@ -182,15 +215,21 @@ private:
         return true;
     }
     void op_end()
-    {
+    {   
+        if(elapsed==token+1){
+            elapsed = 0;
+            phase_end = false;
+            return;
+        }
         printf("#\n");
+        elapsed = 0;
         phase_end = false;
     }
 
 public:
     void init()
     {
-        head = 1;
+        head = 0;
         token = G;
         size = V;
     }
@@ -219,71 +258,102 @@ public:
         assert(current_write_point == size);
     }
 
-    // 传入需要读取的 磁盘的块 的索引
+    // 传入需要读取的 磁盘的块 的索引 vector从0开始索引
     // 返回 已读取的块 的索引
-    void task(vector<int> target, int disk_id)
-    {
-        // if (target.size() == 0) return;
-        // TODO 调用最优化使用磁头token的算法
-        // 第一次出现的目标大于(令牌-64)就进行jump 否则pass至目标 进行读取、
-        int next=1; // 使用to_finds[next]可用得到下一个目标的磁盘地址
-        int tokens = G;
-        int need_move = false;
-        vector<int> found;
-        for (int i = 1; i <= target.size() - 1; i++)
+    void task(vector<int> input_target, int disk_id)
+    {   
+        DEBUG_PRINT(disk_id);
+        DEBUG_PRINT(head);
+        DEBUG_PRINT(elapsed);
+        if (input_target.empty())
         {
-            if (target[i] >= head)
-            {
-                next = i;
-                need_move = true;
-                break;
-            }
-        }
-        if(!need_move){
+            op_end();
+            DEBUG_PRINT(input_target.size());
             return;
         }
-        // to_finds[next]是磁头后 最近的目标块
-        // 距离 target[next]-head)
-        
-        if (target[next] - head + 64 > tokens)
+        // DEBUG_LOG("each block target for this disk");
+        // for (auto blockidx : input_target)
+        // {
+        //     DEBUG_PRINT(blockidx);
+        // }
+        // 如果无任务，直接结束
+        vector<int> target=input_target;
+        // 尝试对input target按离磁头远近重排
+        DEBUG_PRINT(head);
+        auto it = lower_bound(target.begin(), target.end(), head+1);
+        if (it == target.begin() || it == target.end())
         {
-            // 过远的情况
-            
-            operate(JUMP, target[next]);
+            // 所有元素都在磁头之前、之后 都不用排
+            DEBUG_LOG("neednt REarrage");
         }
         else
         {
-            while (elapsed + (target[next] - head) < tokens) // elapsed+距离 < token上限
+            vector<int> first_half(target.begin(), it);
+            vector<int> second_half(it, target.end());
+            target.clear();
+            target.insert(target.end(), second_half.begin(), second_half.end());
+            target.insert(target.end(), first_half.begin(), first_half.end());
+        }
+        // DEBUG_LOG("each block target after rearrange");
+        // for (auto blockidx : target)
+        // {
+        //     DEBUG_PRINT(blockidx);
+        // }
+        // OPT 调用最优化使用磁头token的算法
+        // 第一次出现的目标大于(令牌-64)就进行jump 否则pass至目标 进行读取
+        int idx = 0; // target[idx]可用得到下一个目标的磁盘地址
+        int tokens = G;
+        int need_move = false;
+        vector<int> found;
+        DEBUG_PRINT(target[idx]);
+        // target[0]是磁头后 最近的目标块
+        // 距离 target[0]-head dis = dist<0?dist+8000:dist
+        // 如 target[0]=2 head = 7998 距离应为4 直接减为 -7996
+        int distance = (target[0]-1) - head;
+        if (distance < 0)
+            distance += 8000;
+        DEBUG_PRINT(distance);
+        if (distance + 64 > tokens)
+        {
+            // 过远的情况
+            operate(JUMP, target[0]-1);
+            op_end();
+            return;
+        }
+        else
+        {
+            while (elapsed < tokens) // elapsed+距离 < token上限
             {
-                int read_consume;
-                
-                if (operate(PASS, target[next] - head)) // token足够移动
-                {
-                    
-                    if (operate(READ, 0)) // token足够读取
-                    {
-                        found.push_back(target[next]); // 放入找到的块
-                        if (next + 1 == target.size()) // 尝试找下一个
-                            break;
-                        next++; // 还有其他要找的情况
-                        
-                        continue;
-                    }
-                    else
-                        break;
-                }
-                else
+                if(distance>0){
+                    if (!operate(PASS, distance)) // token不足移动 直接退出循环
                     break;
+                }
+                //现在磁头移动到目标块上了
+                int read_consume;
+                if (!operate(READ, 0)) // token不足读取 退出循环
+                    break;
+                found.push_back(target[idx]); // 放入读到的块
+                idx++;
+                if (idx >= target.size()) // idx已经超过有元素的索引值了
+                    break;
+                // 更新distance 尝试找下一个
+                distance = (target[idx]-1) - head;
+                if (distance < 0)
+                    distance += 8000;
             }
         }
         op_end();
-        //TODO 
-        for(int i:found){//i disk地址
+        for (int i : found)
+        { // i disk地址
             int complete_id = 0;
-            for(int j = 1; j <= 3; j++){// j 副本索引
-                if(disk_id == object[blocks[i]].replica[j]){
-                    for(int k = 1; k <= object[blocks[i]].size; k++){//k 第j副本的第k子块
-                        if(object[blocks[i]].unit[j][k] == i){
+            for (int j = 1; j <= 3; j++)
+            { // j 副本索引
+                if (disk_id == object[blocks[i]].replica[j])
+                {
+                    for (int k = 1; k <= object[blocks[i]].size; k++)
+                    { // k 第j副本的第k子块
+                        if (object[blocks[i]].unit[j][k] == i)
+                        {
                             complete_id = k;
                             break;
                         }
@@ -291,12 +361,11 @@ public:
                 }
                 continue;
             }
-            request[object[blocks[i]].last_request_point].complete[complete_id]  = 1;
+            request[object[blocks[i]].last_request_point].complete[complete_id] = 1;
         }
-        //return found;
+        // return found;
     }
 };
-
 
 Scheduler scheduler;
 Disk disks[MAX_DISK_NUM];
@@ -333,8 +402,8 @@ void delete_action()
     {
         int id = _id[i];
         int current_id = object[id].last_request_point;
-        
-        while (current_id!=0)
+
+        while (current_id != 0)
         {
             // TODE 调用调度器的um查找_id的对象编号 值清空)
             if (request[current_id].is_done == false)
@@ -436,9 +505,12 @@ void read_action()
         request[request_id].is_done = false;
         scheduler.add_request(request_id);
     }
-
-    for(int i=1;i<=N;i++){
-        disks[i].task(scheduler.get_task_for_disk(i),i);
+    // for(auto req_id:scheduler.active_requests){
+    //     DEBUG_PRINT(req_id);
+    // }
+    for (int i = 1; i <= N; i++)
+    {
+        disks[i].task(scheduler.get_task_for_disk(i), i);
     }
     scheduler.req_upload();
 
@@ -518,14 +590,13 @@ void clean()
 
 int main()
 {
-
     scanf("%d%d%d%d%d", &T, &M, &N, &V, &G);
-    // TODO init 磁盘 各种信息 token 空闲链表等等等
-    for (auto& D : disks)
+    // TODO init 磁盘的空闲链表等等等
+    for (auto &D : disks)
     {
         D.init();
     }
-    
+
     for (int i = 1; i <= M; i++)
     {
         for (int j = 1; j <= (T - 1) / FRE_PER_SLICING + 1; j++)
