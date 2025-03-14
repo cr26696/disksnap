bb4e1a2ff34703869f475a67e10d2a6b68f48905
Wwbbbbb <267057047@qq.com>
2025-03-14 17:55:14 +0800

    feat(Disk): 实现对象删除和写入逻辑，优化空闲块管理

--- src/Disk.cpp
+++ src/Disk.cpp
@@ -5,7 +5,7 @@ using namespace std;
 Disk::Disk(int V, int G)
     : head(0), head_s(-1), sizeV(V), tokenG(G), elapsed(0), phase_end(false), blocks(V, 0)
 {
-
+    free_blocks.emplace_back(1, V);
 }
 
 bool Disk::operate(DiskOp op, int param)
@@ -83,38 +83,104 @@ void Disk::op_end()
     phase_end = false;
 }
 
-// void Disk::init(int G, int V)
-// {
-//     head = 0;
-//     token = G;
-//     size = V;
-// }
 
-void Disk::delete_obj(int *units, int size)
-{
-    for (int i = 1; i <= size; i++)
-    {
+/*
+* 删除对象 并进行空闲块合并整理 按区间开头升序排序
+*/
+void Disk::delete_obj(int *units, int object_size){
+    vector<int> temp_free_units;// 空闲碎片
+    vector<pair<int, int>> temp_free_blocks;// 空闲区间
+    for(int i = 1; i <= object_size; i++){
+        assert(blocks[units[i]] != 0);
         blocks[units[i]] = 0;
+        temp_free_units.emplace_back(units[i]);
     }
-}
-//写入一个副本
-//往map中插入
-void Disk::write_obj(int object_id, int *obj_units, int size)
-{
-    int current_write_point = 0;
-    for (int i = 1; i <= size; i++)
-    {
-        if (blocks[i] == 0)
-        {
-            blocks[i] = object_id;
-            obj_units[++current_write_point] = i;
-            if (current_write_point == size)
-            {
-                break;
-            }
+    sort(temp_free_units.begin(), temp_free_units.end());// 将碎片块排序
+    int start = temp_free_units[0], end = temp_free_units[0];
+    for(int i = 1; i < temp_free_units.size(); i++){// 将空闲碎片合并为空闲区间
+        if(temp_free_units[i] == end + 1){
+            end = temp_free_units[i];
+        }
+        else if(temp_free_units[i] != end + 1 || i == temp_free_units.size() - 1){// 若当前块与前一个块不连续或为最后一个区间
+            temp_free_blocks.emplace_back(start, end);
+            start = temp_free_units[i];
+            end = temp_free_units[i];
         }
     }
-    assert(current_write_point == size);
+    for(auto& block : temp_free_blocks) {
+        free_blocks.push_back(block);
+    }
+    free_blocks.sort();
+    for(auto current_block = free_blocks.begin(); current_block != free_blocks.end();){
+        auto next_block = next(current_block);
+        if(next_block == free_blocks.end()) break;
+        assert(current_block->second == next_block->first - 1);// 理论上不会大于
+        if(current_block->second == next_block->first - 1){// 表示上一个空闲区别和下一个空闲区间相近 需要进行合并
+            current_block->second = next_block->second;
+            free_blocks.erase(next_block);
+        }
+        else{
+            current_block++;
+        }
+    }    
+}
+
+
+/*
+* 写入对象 优先满足连续存储 若无法连续存储 则进行分块存储
+*/
+void Disk::write_obj(int object_id, int *obj_units, int object_size){
+    int current_write_point = 0;
+    int temp_write_point = 0;
+    typedef list<pair<int, int>>::iterator p_it;// 记录暂存时所选择空闲块的迭代器
+    vector<pair<p_it, int>> temp_operate;//记录暂存空闲块与块大小
+    for(auto it = free_blocks.begin(); it != free_blocks.end(); it++){
+        int free_block_size = it->second - it->first + 1;// 当前空闲块的空间
+        // 找到可连续存储块时 放弃暂存 直接存储
+        if(free_block_size >= object_size){
+            for(int i = 0; i < object_size; i++){// 从0开始 因为可从首地址开始存储
+                assert(blocks[it->first + i] == 0);
+                blocks[it->first + i] = object_id;// 存入磁盘
+                obj_units[++current_write_point] = it->first + i;// 标记对象块位置
+            }
+            if(free_block_size == object_size){// 若空闲块被填满，则删除该空闲块节点
+                free_blocks.erase(it);
+            }
+            else{// 若空闲块没有被填满，则修改该空闲块区间头
+                it->first += object_size;
+            }
+            assert(current_write_point == object_size);
+            return;
+        }
+        // 进行分块暂存记录 以防无法连续存储
+        if(temp_write_point != object_size){
+            int not_write_size = object_size - temp_write_point;// 仍未存入的对象块大小
+            // 标记暂存空间块使用情况 填满空闲块：填入剩余对象块剩余空间
+            if(not_write_size >= free_block_size){
+                temp_operate.emplace_back(it, free_block_size);
+                temp_write_point += free_block_size;
+            }
+            else{
+                temp_operate.emplace_back(it, not_write_size);
+                temp_write_point += not_write_size;
+            }
+        }        
+    }
+    // 使用暂存操作
+    for(auto it : temp_operate){
+        for(int i = 0; i < it.second; i++){
+            assert(blocks[it.first->first + i] == 0);
+            blocks[it.first->first + i] = object_id;// 存入磁盘
+            obj_units[++current_write_point] = it.first->first + i;// 标记对象块位置
+        }
+        int free_block_size = it.first->second - it.first->first + 1;
+        if (it.second == free_block_size) { // 如果填满了整个空闲块，则删除该空闲块
+            free_blocks.erase(it.first);
+        } else { // 否则修改空闲块的起始位置
+            it.first->first += it.second;
+        }        
+    }
+    assert(current_write_point == object_size);
 }
 
 void Disk::task(std::vector<int> input_target, int disk_id)
@@ -202,12 +268,14 @@ void Disk::task(std::vector<int> input_target, int disk_id)
     }
 }
 
-bool Disk::hasSpace(int size) {
-    // 检查磁盘是否有足够的空间
-    return true; // 伪代码实现
+/*
+*计算总剩余空间大小
+*/
+int Disk::numberOfFreeBlocks_() {
+    int all_free_size = 0;
+    for(auto it : free_blocks){
+        all_free_size += it.second - it.first + 1;
+    }
+    return all_free_size;
 }
 
-void Disk::store(int id, int tag, int size) {
-    // 存储对象的逻辑
-    // 伪代码实现
-}
